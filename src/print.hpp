9230d48b11f1d84a937f5dd476ac9846eb2254be
jurriaan <jurriaan@laptopinator.localdomain>
2020-01-21 12:45:49 +0100

    Downgraded opengl to version 4.3

--- src/print.hpp
+++ src/print.hpp
@@ -12,6 +12,7 @@
 
 #include <sstream>
 #include <iostream>
+#include <typeinfo>
 
 
 template <typename...>
--- src/sdl/window.cpp
+++ src/sdl/window.cpp
@@ -64,7 +64,7 @@ Window pcs::createOpenGLWindow( const std::string& title, int width, int height 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                         SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
     window.glContext = SDL_GL_CreateContext(window.sdlData);
 
